82356378d1e397fc27a9e67c51710019fec7c36d
Javier Rodriguez Posada <jrposada.dev@gmail.com>
2021-09-15 21:35:32 +0200

    var exmaple

--- App.mjs
+++ App.mjs
@@ -1,4 +1,21 @@
-import mmm, { Hello as NotHello } from './myEsmModule.mjs'
+var myGlobal = "I'm global... am I?"
+var myVar = "I'm a var"
 
-NotHello()
-mmm()
+if (false) {
+    var myVar = 'intercepted'
+}
+
+function What() {
+    console.log(`What? Global: ${myGlobal}`)
+    console.log(`What? Before: ${myVar}`)
+    var myVar = 'what is this?'
+    console.log(`What? After: ${myVar}`)
+}
+
+console.log(`Before myVar: ${myVar}`)
+What()
+console.log(`After myVar: ${myVar}`)
+
+console.log(`Before myHoistedVar: ${myHoistedVar}`)
+var myHoistedVar = "I'been hoisted!"
+console.log(`After myHoistedVar: ${myHoistedVar}`)
