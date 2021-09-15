ce5614b37f31928be3a3281b6f2490c165ccbc6d
Javier Rodriguez Posada <jrposada.dev@gmail.com>
2021-09-15 21:49:17 +0200

    let example

---
+++ App-let.mjs
@@ -0,0 +1,21 @@
+let myGlobal = "I'm global... am I?"
+let myVar = "I'm a var"
+
+if (true) {
+    let myVar = 'intercepted'
+}
+
+function What() {
+    console.log(`What? Global: ${myGlobal}`)
+    // console.log(`What? Before: ${myVar}`)
+    let myVar = 'what is this?'
+    console.log(`What? After: ${myVar}`)
+}
+
+console.log(`Before myVar: ${myVar}`)
+What()
+console.log(`After myVar: ${myVar}`)
+
+// console.log(`Before myHoistedVar: ${myHoistedVar}`)
+let myHoistedVar = "I'been hoisted!"
+console.log(`After myHoistedVar: ${myHoistedVar}`)
